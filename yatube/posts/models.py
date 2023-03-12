174554765d3764d8ae188f2f75c201bc451352af
Сайгушев Дамир <saigh06@gmail.com>
2023-03-12 17:40:22 +0200

    hm-done-reborne-3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -85,13 +85,5 @@ class Follow(models.Model):
         related_name='following'
     )
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=('user', 'author',),
-                name='unique_follow'
-            ),
-        ]
-
     def __str__(self):
         return self.user
